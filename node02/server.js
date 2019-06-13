d88c4e96a9c9266ef2858c4f4fd64adb1ec20813
dunshaw <dunshaw520@163.com>
2019-06-13 21:30:33 +0800

    12

--- node02/server.js
+++ node02/server.js
@@ -4,6 +4,8 @@ const app = express()    //express 实例化
 
 // 简单的api接口
 app.get('/user/login',(req,res)=>{
+    // 接收get参数  req.query
+    console.log(req.query);
     console.log('你好');
     res.send({'err':0,'msg':'okokok'});
 })
